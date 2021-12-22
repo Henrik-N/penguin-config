e7a1402749e58962165e3a81682057227419d4f2
Henrik Nilsson <n.henrik.g.nilsson@gmail.com>
2021-12-22 12:09:22 +0100

    Moved derive macro package into penguin-config's workspace
    Moved derive macro package into the same workspace workspace instead having it be a seperate git submodule. I misunderstood how cargo workspaces worked earlier.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,15 @@ mod prelude {
     use std::io::Read;
 
     pub use serde::Deserialize;
-    pub use crate::traits::PenguinConfig;
+    pub use serde::Serialize;
     pub use crate::deserializer::Deserializer;
+    pub use crate::serializer::Serializer;
 
-    #[cfg(feature = "penguin-config-derive")]
+    pub use crate::traits::PenguinConfig;
+    pub use crate::traits::PenguinConfigGenerate;
+
     pub use penguin_config_derive::PenguinConfigFile;
+    pub use penguin_config_derive::PenguinConfigGenerate;
 
     pub fn read_json(file_path: &str) -> String {
         let mut file = File::open(file_path).expect("couldn't open file");
@@ -23,12 +27,17 @@ mod traits {
     pub trait PenguinConfig {
         fn read_config() -> Self;
     }
+
+    pub trait PenguinConfigGenerate {
+        fn generate_penguin_config_file();
+    }
 }
 
 mod deserializer {
     use std::marker::PhantomData;
     use crate::read_json;
 
+    /// let t: T = Deserializer::file_path("some_path.json").deserialize();
     pub struct Deserializer<'a, T: serde::Deserialize<'a>> {
         json: String,
         phantom_data: PhantomData<&'a T>,
@@ -43,9 +52,50 @@ mod deserializer {
         }
 
         pub fn deserialize(&'a self) -> T {
-            let config: T = serde_json::from_str(&self.json).expect("couldn't deserialize");
+            let config: T = serde_json::from_str(&self.json)
+                .expect(&format!("couldn't deserialize string: [{}]", &self.json));
             config
         }
     }
 }
+
+mod serializer {
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::Path;
+
+    /// Serializer::file_path("some_path.json").serialize(&T::default());
+    pub struct Serializer {
+        path: String,
+    }
+    
+    impl Serializer {
+        pub fn file_path(file_path: &str) -> Self {
+
+            // create folder(s) if not already present
+            let path = Path::new(file_path);
+            let prefix = path.parent().expect("couldn't find parent dirs");
+            std::fs::create_dir_all(prefix).expect("couldn't create parent dirs");
+
+            Self { path: file_path.to_owned(), }
+        }
+
+        pub fn serialize<T: serde::Serialize>(&self, data: &T) {
+            let json = serde_json::to_string(data).expect("couldn't turn data into json");
+
+            let mut file = File::create(&self.path)
+                .expect(&format!("couldn't create file at path: {}", &self.path));
+
+            file.write_all(json.as_bytes())
+                .expect(&format!("couldn't write json to file {}", &self.path));
+        }
+    }
+}
+
+
+
+
+
+
+
 
